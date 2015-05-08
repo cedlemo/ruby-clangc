45308d170a82fe15e061cee1ec3fac8f29f9e036
cedlemo <cedlemo@gmx.com>
2015-05-08 11:35:11 +0200

    Remove git reference to reinstallgem.sh

--- reinstallgem.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gem uninstall clangc
-gem build clangc.gemspec
-gem install --debug --verbose clangc-0.0.1.gem -- --enable-debug
-ruby test/Index_tests.rb
-ruby test/TranslationUnit_tests.rb
-rm clangc-0.0.1.gem
