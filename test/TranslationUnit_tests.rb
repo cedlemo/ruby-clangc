f7c44257ddd96fb1ead810a3a679f9cd36f1c81c
cedlemo <cedlemo@gmx.com>
2015-05-09 18:11:25 +0200

    Add tests for inexistant source or ast file

--- test/TranslationUnit_tests.rb
+++ test/TranslationUnit_tests.rb
@@ -6,25 +6,41 @@ class TestTranslationUnitCreation < MiniTest::Test
   def setup
     @cindex = Clangc::Index.new(false, false)
     @source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+    @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
     @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
   end
   def test_create_TU_from_source_file
     tu = @cindex.create_translation_unit_from_source_file(@source_file,[""])
     assert_instance_of Clangc::TranslationUnit, tu 
   end
+  def test_fail_create_TU_from_source_file
+    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[""])
+    assert_nil tu 
+  end
   def test_create_TU_from_source_file_empty_cmdline
     tu = @cindex.create_translation_unit_from_source_file(@source_file,[])
     assert_instance_of Clangc::TranslationUnit, tu 
+  end
+  def test_fail_create_TU_from_source_file_empty_cmdline
+    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[])
+    assert_nil tu 
   end
   def test_create_TU_from_source_file_in_cmdline
     tu = @cindex.create_translation_unit_from_source_file("",[@source_file])
     assert_instance_of Clangc::TranslationUnit, tu 
   end
+  def test_fail_create_TU_from_source_file_in_cmdline
+    tu = @cindex.create_translation_unit_from_source_file("",[@bad_file])
+    assert_nil tu 
+  end
   def test_create_TU
     tu = @cindex.create_translation_unit(@ast_file)
     assert_instance_of Clangc::TranslationUnit, tu 
   end
-  # return an error code if the Translation Unit creation fail
+  def test_fail_create_TU
+    tu = @cindex.create_translation_unit(@bad_file)
+    assert_nil tu 
+  end  # return an error code if the Translation Unit creation fail
   def test_create_TU2
 
   end
@@ -32,6 +48,11 @@ class TestTranslationUnitCreation < MiniTest::Test
     options = Clangc::TranslationUnit_Flags::None
     tu = @cindex.parse_translation_unit(@source_file,[], options)
     assert_instance_of Clangc::TranslationUnit, tu
+  end
+  def test_fail_parse_TU
+    options = Clangc::TranslationUnit_Flags::None
+    tu = @cindex.parse_translation_unit(@bad_file,[], options)
+    assert_nil tu
   end
   # return an error code if the Translation Unit creation fail
   def test_parse_TU2
