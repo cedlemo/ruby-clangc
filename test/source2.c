a0ee33e0fb61d91c83c4f526cb74e4609721dd3a
cedlemo <cedlemo@gmx.com>
2015-05-16 19:26:14 +0200

    add C source file with one error for tests

---
+++ test/source2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct data_t {
+  int index;
+  char * array[256];
+  } data_t
+
+int
+stupid_function (double d) {
+  printf("This is a stupid function\n");
+  return 0;
+}
+
+int
+main (int argc, char **argv) {
+
+  return 0;
+}
+
