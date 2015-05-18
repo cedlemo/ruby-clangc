8be71dbdd5926726947d11b7caf7811d996a4cf3
cedlemo <cedlemo@gmx.com>
2015-05-18 19:14:27 +0200

    Set header options to the Clang parser(to be improved)

--- test/TranslationUnit_tests.rb
+++ test/TranslationUnit_tests.rb
@@ -12,29 +12,30 @@ class TestTranslationUnitCreation < MiniTest::Test
     # Inexistant file
     @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
     @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
+    @clang_headers_path = "-I/usr/lib/clang/3.6.0/include/"
   end
   def test_create_TU_from_source_file
-    tu = @cindex.create_translation_unit_from_source_file(@source_file,[""])
+    tu = @cindex.create_translation_unit_from_source_file(@source_file,[@clang_headers_path])
     assert_instance_of Clangc::TranslationUnit, tu 
   end
   def test_fail_create_TU_from_source_file
-    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[""])
+    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[@clang_headers_path])
     assert_nil tu 
   end
   def test_create_TU_from_source_file_empty_cmdline
-    tu = @cindex.create_translation_unit_from_source_file(@source_file,[])
+    tu = @cindex.create_translation_unit_from_source_file(@source_file,[@clang_headers_path])
     assert_instance_of Clangc::TranslationUnit, tu 
   end
   def test_fail_create_TU_from_source_file_empty_cmdline
-    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[])
+    tu = @cindex.create_translation_unit_from_source_file(@bad_file,[@clang_headers_path])
     assert_nil tu 
   end
   def test_create_TU_from_source_file_in_cmdline
-    tu = @cindex.create_translation_unit_from_source_file("",[@source_file])
+    tu = @cindex.create_translation_unit_from_source_file("",[@source_file,@clang_headers_path])
     assert_instance_of Clangc::TranslationUnit, tu 
   end
   def test_fail_create_TU_from_source_file_in_cmdline
-    tu = @cindex.create_translation_unit_from_source_file("",[@bad_file])
+    tu = @cindex.create_translation_unit_from_source_file("",[@bad_file,@clang_headers_path])
     assert_nil tu 
   end
   def test_create_TU
@@ -57,23 +58,23 @@ class TestTranslationUnitCreation < MiniTest::Test
   end
   def test_parse_TU
     options = Clangc::TranslationUnit_Flags::None
-    tu = @cindex.parse_translation_unit(@source_file,[], options)
+    tu = @cindex.parse_translation_unit(@source_file,[@clang_headers_path], options)
     assert_instance_of Clangc::TranslationUnit, tu
   end
   def test_fail_parse_TU
     options = Clangc::TranslationUnit_Flags::None
-    tu = @cindex.parse_translation_unit(@bad_file,[], options)
+    tu = @cindex.parse_translation_unit(@bad_file,[@clang_headers_path], options)
     assert_nil tu
   end
   # return an error code if the Translation Unit creation fail
   def test_parse_TU2
     options = Clangc::TranslationUnit_Flags::None
-    tu = @cindex.parse_translation_unit2(@source_file,[], options)
+    tu = @cindex.parse_translation_unit2(@source_file,[@clang_headers_path], options)
     assert_instance_of Clangc::TranslationUnit, tu
   end
   def test_failparse_TU2
     options = Clangc::TranslationUnit_Flags::None
-    tu = @cindex.parse_translation_unit2(@bad_file,[], options)
+    tu = @cindex.parse_translation_unit2(@bad_file,[@clang_headers_path], options)
     assert_equal Integer, tu.class.superclass
     assert_equal Clangc::ErrorCode::Failure, tu
   end
@@ -88,13 +89,14 @@ class TestTranslationUnitCreation < MiniTest::Test
     # Inexistant file
     @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
     @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
+    @clang_headers_path = "-I/usr/lib/clang/3.6.0/include/"
   end
   def test_TU_get_num_diagnostic_zero
-    tu = @cindex.create_translation_unit_from_source_file(@source_file,[""])
+    tu = @cindex.create_translation_unit_from_source_file(@source_file,[@clang_headers_path])
     assert_equal 0, tu.diagnostics_num
   end
   def test_TU_get_num_diagnostic_one
-    tu = @cindex.create_translation_unit_from_source_file(@source_file_one_error,[""])
-    assert_equal 0, tu.diagnostics_num
+    tu = @cindex.create_translation_unit_from_source_file(@source_file_one_error,[@clang_headers_path])
+    assert_equal 1, tu.diagnostics_num
   end  
 end
