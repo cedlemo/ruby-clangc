80d11e47856ea6b0e94ce48a5a12092cc240c8ad
cedlemo <cedlemo@gmx.com>
2015-05-25 10:56:59 +0200

    Implement Clangc::TranslationUnit#diagnostics

--- lib/clangc.rb
+++ lib/clangc.rb
@@ -2,4 +2,13 @@ require "clangc/clangc"
 
 module Clangc
   VERSION = "0.0.0"
+  class TranslationUnit
+    def diagnostics
+      ds = []
+      for i in 0..(diagnostics_num - 1) do
+        ds << diagnostic(i)
+      end
+      ds
+    end
+  end
 end
