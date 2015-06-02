16f0a3cb8ca6d07296ab49b5dd55679a326eadbd
cedlemo <cedlemo@gmx.com>
2015-06-02 18:33:17 +0200

    Handle tests for Clangc module functions in the reinstall script

--- tools/reinstallgem.sh
+++ tools/reinstallgem.sh
@@ -4,6 +4,7 @@ W_DIR=$(pwd)
 gem uninstall clangc
 gem build $DIR/../clangc.gemspec
 gem install --debug --verbose $W_DIR/clangc-0.0.1.gem -- --enable-debug
+ruby $DIR/../test/Clangc_tests.rb --pride
 ruby $DIR/../test/Index_tests.rb --pride
 ruby $DIR/../test/TranslationUnit_tests.rb --pride
 ruby $DIR/../test/Diagnostic_tests.rb --pride
