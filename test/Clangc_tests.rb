888f9f87fded517553df8462166b0d4320dc338a
cedlemo <cedlemo@gmx.com>
2015-06-10 19:15:22 +0200

    Add Clangc#default_diagnostic_display_options function tests

--- test/Clangc_tests.rb
+++ test/Clangc_tests.rb
@@ -6,4 +6,9 @@ class TestClangcMethods < MiniTest::Test
   def test_clangc_version
     assert_instance_of String, Clangc.version
   end
+  def test_clangc_default_diagnostic_display_options
+    assert_instance_of Fixnum, Clangc.default_diagnostic_display_options
+# TODO add a test with 1|2|8
+#    assert_equal 1, Clangc.default_diagnostic_display_options
+  end
 end
