876158c27ae1ebede514a356aba774ab97663015
cedlemo <cedlemo@gmx.com>
2015-06-13 10:47:53 +0200

    Fix typo

--- ext/clangc/_clangc_functions.h
+++ ext/clangc/_clangc_functions.h
@@ -26,5 +26,5 @@ m_clangc_get_default_diagnostic_display_options(VALUE);
 VALUE
 m_clangc_get_default_editing_translation_unit_options(VALUE);
 VALUE
-m_clangc_get_default_code_complete_options(VLAUE);
+m_clangc_get_default_code_complete_options(VALUE);
 #endif
