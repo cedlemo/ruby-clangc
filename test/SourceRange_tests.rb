fe32a8c0009999644395ed4d6bc91b2df804c959
cedlemo <cedlemo@gmx.com>
2015-06-20 15:04:33 +0200

    Improve tests for Clangc::SourceRange#is_equal

--- test/SourceRange_tests.rb
+++ test/SourceRange_tests.rb
@@ -14,6 +14,8 @@ class TestSourceRange < MiniTest::Test
     @source_file_one_error = "#{File.expand_path(File.dirname(__FILE__))}/source2.c"
     # C source code with two error
     @source_file_two_errors = "#{File.expand_path(File.dirname(__FILE__))}/source3.c"
+    # C source code with three error
+    @source_file_three_errors = "#{File.expand_path(File.dirname(__FILE__))}/source4.c"
     # Inexistant file
     @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
     @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
@@ -40,17 +42,20 @@ class TestSourceRange < MiniTest::Test
     range_number = tu.diagnostics[1].num_ranges
     source_range_1 = diagnostics[1].source_ranges[range_number - 1]
     source_range_2 = diagnostics[1].source_ranges[range_number - 1]
-    assert_equal true, source_range_1.is_equal source_range_2
+    assert_equal true, source_range_1.is_equal(source_range_2)
     assert_equal false, source_range_1 == source_range_2
   end
   def test_SourceRange_is_equal_false
-    tu = @cindex.create_translation_unit_from_source_file(@source_file_two_errors,@clang_headers_path)
+    tu = @cindex.create_translation_unit_from_source_file(@source_file_three_errors,@clang_headers_path)
     diagnostics = tu.diagnostics
-    range_number = tu.diagnostics[1].num_ranges
+    
+    range_number = diagnostics[1].num_ranges
     source_range_1 = diagnostics[1].source_ranges[range_number - 1]
-    source_range_2 = diagnostics[1].source_ranges[range_number - 2]
-    assert_equal false, source_range_1.is_equal source_range_2
-    assert_equal false, source_range_1 == source_range_2
 
+    range_number = diagnostics[2].num_ranges
+    source_range_2 = diagnostics[2].source_ranges[range_number - 1]
+    
+    assert_equal false, source_range_1.is_equal(source_range_2)
+    assert_equal false, source_range_1 == source_range_2
   end
 end
---
+++ test/source4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct data_t {
+  int index;
+  char * array[256];
+  } data_t
+
+int
+stupid_function (double d) {
+  printf("This is a stupid function\n");
+  return 0;
+}
+
+a = 10;
+
+b = 30;
+int
+main (int argc, char **argv) {
+
+  return 0;
+}
+
