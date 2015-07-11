4d3f4c56c1e14289361f618abb99473feb56ecfb
cedlemo <cedlemo@gmx.com>
2015-07-11 19:05:11 +0200

    Add Cursor test in the list of test scripts

--- tools/reinstallgem.sh
+++ tools/reinstallgem.sh
@@ -4,7 +4,8 @@ W_DIR=$(pwd)
 gem uninstall clangc
 gem build $DIR/../clangc.gemspec
 gem install --debug --verbose $W_DIR/clangc-0.0.1.gem -- --enable-debug
-for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" "SourceLocation"; do
+for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" "SourceLocation" "Cursor"; do
+  echo "############> ${N}_tests.rb <###############"
   ruby "${DIR}/../test/${N}_tests.rb" --pride
 done
 rm $W_DIR/clangc-0.0.1.gem
