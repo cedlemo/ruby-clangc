344b74da7b5979abd6cc9f899e8d998c76c42c83
cedlemo <cedlemo@gmx.com>
2015-07-11 19:04:35 +0200

    Add test for Clangc::Cursor#is_null method

---
+++ test/Cursor_tests.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require "minitest/autorun"
+require "clangc"
+require "fileutils"
+class TestTranslationUnitUsage < MiniTest::Test
+  def setup
+    @cindex = Clangc::Index.new(false, false)
+    # Good C test file
+    @source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+    # C source code with one error
+    @source_file_one_error = "#{File.expand_path(File.dirname(__FILE__))}/source2.c"
+    # Inexistant file
+    @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
+    @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
+    system *%W(clang -emit-ast -o #{@ast_file} #{@source_file})
+    @clang_headers_path = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
+  end
+  def teardown
+    FileUtils.rm_f(@ast_file)
+  end
+  def test_TU_get_cursor
+    tu = @cindex.create_translation_unit_from_source_file(@source_file, @clang_headers_path)
+    assert_instance_of Clangc::Cursor, tu.cursor
+  end
+  def test_Cursor_is_null
+    cursor = Clangc.null_cursor
+    assert_equal true, cursor.is_null
+  end
+  def test_Cursor_is_not_null
+    tu = @cindex.create_translation_unit_from_source_file(@source_file, @clang_headers_path)
+    assert_equal false, tu.cursor.is_null
+  end
+end
