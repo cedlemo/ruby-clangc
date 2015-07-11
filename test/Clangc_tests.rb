eaf48e6232d5e75c1eb37cbcc7733dce14e81aaf
cedlemo <cedlemo@gmx.com>
2015-07-11 15:54:01 +0200

    Add test for Clangc.visit_children* methods

--- test/Clangc_tests.rb
+++ test/Clangc_tests.rb
@@ -28,4 +28,46 @@ class TestClangcMethods < MiniTest::Test
   def test_clangc_null_cursor
     assert_instance_of Clangc::Cursor, Clangc.null_cursor
   end
+  def test_clang_visit_children_with_block
+    index = Clangc::Index.new(false, false)
+    source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+    clang_headers_path = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
+    tu = index.create_translation_unit_from_source_file(source_file, 
+                                                         clang_headers_path)
+    cursor = tu.cursor
+    Clangc.visit_children_with_block(cursor) do |cursor, parent| 
+      assert_instance_of Clangc::Cursor, cursor
+      assert_instance_of Clangc::Cursor, parent
+    end
+  end
+  def test_clang_visit_children_with_proc
+    index = Clangc::Index.new(false, false)
+    source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+    clang_headers_path = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
+    tu = index.create_translation_unit_from_source_file(source_file, 
+                                                         clang_headers_path)
+    cursor = tu.cursor
+    myproc = proc do |cursor, parent| 
+      assert_instance_of Clangc::Cursor, cursor
+      assert_instance_of Clangc::Cursor, parent
+    end
+    Clangc.visit_children_with_proc(cursor, myproc)
+  end
+  def test_clang_visit_children
+    index = Clangc::Index.new(false, false)
+    source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+    clang_headers_path = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
+    tu = index.create_translation_unit_from_source_file(source_file, 
+                                                         clang_headers_path)
+    cursor = tu.cursor
+    Clangc.visit_children(cursor: cursor) do |cursor, parent| 
+      assert_instance_of Clangc::Cursor, cursor
+      assert_instance_of Clangc::Cursor, parent
+    end
+    myproc = proc do |cursor, parent| 
+      assert_instance_of Clangc::Cursor, cursor
+      assert_instance_of Clangc::Cursor, parent
+    end
+    Clangc.visit_children(cursor: cursor, visitor: myproc)
+  end
 end
