847d9c9c28f1bdb37a650b1d9e1d23548c324045
cedlemo <cedlemo@gmx.com>
2015-08-20 11:24:44 +0200

    Add new sample

--- samples/visit_children_1.rb
+++ samples/visit_children_1.rb
@@ -12,9 +12,33 @@ tu = index.create_translation_unit_from_source_file(source_file,
 exit unless tu
 cursor = tu.cursor
 
+def pretty_print(cursor_kind, cursor)
+  printf("%s %s line %d, char %d\n",
+         cursor_kind,
+         cursor.spelling,
+         cursor.location.spelling[1],
+         cursor.location.spelling[2])
+end
+
 Clangc.visit_children(cursor: cursor) do |cursor, parent| 
-  source_location = cursor.location
-  puts "File = #{source_location.spelling[0]}, line #{source_location.spelling[1]}, column #{source_location.spelling[2]}"
-  #source_range = cursor.extent
-  puts cursor.spelling
+  if cursor.location.spelling[0].name == source_file
+    case cursor.kind 
+    when Clangc::CursorKind::Typedefdecl
+      pretty_print("typedef     ", cursor)
+    when Clangc::CursorKind::Structdecl
+      pretty_print("structure   ", cursor)
+    when Clangc::CursorKind::Enumdecl
+      pretty_print("Enumeration ", cursor)
+    when Clangc::CursorKind::Uniondecl
+      pretty_print("Union       ", cursor)
+    when Clangc::CursorKind::Functiondecl
+      pretty_print("Function    ", cursor)
+      arguments = cursor.type.arg_types
+      puts "\t#{arguments.size} argument(s)"
+        arguments.each do |a|
+        puts "\t-\t" + a.spelling 
+      end
+    end
+  end
+  Clangc::ChildVisitResult::Recurse
 end
