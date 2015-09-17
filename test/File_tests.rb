b501796d1fb70e2029ee74d6e359c9aabddb40c0
cedlemo <cedlemo@gmx.com>
2015-09-17 19:15:07 +0200

    Use ClangcUtils module

--- test/File_tests.rb
+++ test/File_tests.rb
@@ -2,45 +2,47 @@
 require "minitest/autorun"
 require "clangc"
 require "fileutils"
+require "./clangc_utils.rb"
 
 class TestFile < MiniTest::Test
+  include ClangcUtils
   def setup
     @cindex = Clangc::Index.new(false, false)
-    # Good C test file
-    @source_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
-    # C source code with one error
-    @source_file_one_error = "#{File.expand_path(File.dirname(__FILE__))}/source2.c"
-    # Inexistant file
-    @bad_file = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
-    @source_file_with_include_guard = "#{File.expand_path(File.dirname(__FILE__))}/include_guarded_header.h"
-    @ast_file = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
-    system *%W(clang -emit-ast -o #{@ast_file} #{@source_file})
-    @clang_headers_path = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
-    @tu = @cindex.create_translation_unit_from_source_file(@source_file, @clang_headers_path)
+#    # Good C test file
+#    SOURCE_FILE = "#{File.expand_path(File.dirname(__FILE__))}/source1.c"
+#    # C source code with one error
+#    SOURCE_FILE_ONE_ERROR = "#{File.expand_path(File.dirname(__FILE__))}/source2.c"
+#    # Inexistant file
+#    BAD_FILE = "#{File.expand_path(File.dirname(__FILE__))}/qsdfqsdf.c"
+#    SOURCE_FILE_WITH_INCLUDE_GUARD = "#{File.expand_path(File.dirname(__FILE__))}/include_guarded_header.h"
+#    AST_FILE = "#{File.expand_path(File.dirname(__FILE__))}/source1.ast"
+    system *%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE})
+#    CLANG_HEADERS_PATH = Dir.glob("/usr/lib/clang/*/include").collect {|x| "-I#{x}"}
+    @tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE, CLANG_HEADERS_PATH)
   end
   def teardown
-    FileUtils.rm_f(@ast_file)
+    FileUtils.rm_f(AST_FILE)
   end
   def test_File_get_name
-    file = @tu.file(@source_file)
-    assert_equal @source_file, file.name
+    file = @tu.file(SOURCE_FILE)
+    assert_equal SOURCE_FILE, file.name
   end
   def test_File_time
-    file = @tu.file(@source_file)
-    assert_equal File.mtime(@source_file).asctime, file.mtime.asctime
+    file = @tu.file(SOURCE_FILE)
+    assert_equal File.mtime(SOURCE_FILE).asctime, file.mtime.asctime
   end
   def test_File_multiple_include_guarded_false
-    file = @tu.file(@source_file)
+    file = @tu.file(SOURCE_FILE)
     assert_equal false, file.is_multiple_include_guarded
   end
   def test_File_multiple_include_guarded_true
-    tu = @cindex.create_translation_unit_from_source_file(@source_file_with_include_guard, @clang_headers_path)
-    file = tu.file(@source_file_with_include_guard)
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_WITH_INCLUDE_GUARD, CLANG_HEADERS_PATH)
+    file = tu.file(SOURCE_FILE_WITH_INCLUDE_GUARD)
     assert_equal true, file.is_multiple_include_guarded
   end
   def test_File_equal_true
-    file = @tu.file(@source_file)
-    another_file_object = @tu.file(@source_file)
+    file = @tu.file(SOURCE_FILE)
+    another_file_object = @tu.file(SOURCE_FILE)
     assert_equal false, file == another_file_object
     assert_equal true, file.is_equal(another_file_object)
   end
