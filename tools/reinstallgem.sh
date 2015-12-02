b11f217c2662e2af58e8bf723e30918189cd6d28
cedlemo <cedlemo@gmx.com>
2015-12-02 10:29:55 +0100

    Make sure the gem testing installation is a user installation and not system wide

--- tools/reinstallgem.sh
+++ tools/reinstallgem.sh
@@ -3,7 +3,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 W_DIR=$(pwd)
 gem uninstall clangc
 gem build $DIR/../clangc.gemspec
-gem install --debug --verbose $W_DIR/clangc-0.0.1.gem -- --enable-debug
+gem install --user-install --debug --verbose $W_DIR/clangc-0.0.1.gem -- --enable-debug
 for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" "SourceLocation" "Cursor" "Type"; do
   echo ""
   echo "############> ${N}_tests.rb <###############"
