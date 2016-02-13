8299fb15d41763493f5eb158544ae56301a5bfad
cedlemo <cedlemo@gmx.com>
2016-02-13 11:10:01 +0100

    Finish sourceparser.rb sample

--- samples/sourceparser.rb
+++ samples/sourceparser.rb
@@ -4,26 +4,33 @@ require "fileutils"
 PATH = File.expand_path(File.dirname(__FILE__))
 
 class SourceParser
+  attr_reader :index, :source_file, :base_dir, 
+              :translation_unit, :diagnostics
   def initialize(source_file, base_dir = nil, lang = "c")
     @source_file = source_file
     @base_dir = base_dir
     include_libs = build_default_include_libs
     args = ["-x", lang] + include_libs
     @index = Clangc::Index.new(false, false)
-    @translation_unit = @index.create_translation_unit_from_source_file(source_file, args)
-    return nil unless @translation_unit
+    @translation_unit = @index.create_translation_unit_from_source_file(source_file,
+                                                                        args)
+    @diagnostics = @translation_unit.diagnostics if @translation_unit
   end
   
   def parse
     cursor = @translation_unit.cursor
     Clangc.visit_children(cursor: cursor) do |cxcursor, parent|
-      if cursor_in_main_file?(cxcursor)
-        puts cxcursor.spelling if cxcursor.kind == Clangc::CursorKind::FUNCTION_DECL
+      if block_given?
+        yield(@translation_unit, cxcursor, parent)
+      else
+        puts "Please provide a block"
       end
-      Clangc::ChildVisitResult::RECURSE
     end
   end
   
+  # Check if the cursor given in argument focus on 
+  # the file we want to parse and not on included
+  # headers
   def cursor_in_main_file?(cursor)
     cursor_file = cursor.location.spelling[0]
     main_file = @translation_unit.file(@source_file)
@@ -32,6 +39,8 @@ class SourceParser
 
   private
 
+  # Add the directories where the default headers files
+  # for the standards libs can be found
   def build_default_include_libs
     header_paths = []
     if @base_dir && Dir.exist?(@base_dir)
@@ -48,7 +57,29 @@ class SourceParser
   end
 end
 
+class CSourceParser < SourceParser
+  def initialize(source_file, base_dir = nil)
+    super(source_file, base_dir, "c")
+  end
+end
+
+class CPPSourceParser < SourceParser
+  def initialize(source_file, base_dir = nil)
+    super(source_file, base_dir, "c++")
+  end
+end
+
 source = "#{PATH}/../tools/clang-3.4/Index.h"
 
-cl34 = SourceParser.new(source)
-cl34.parse
+cl34 = CSourceParser.new(source)
+
+unless cl34.translation_unit
+  puts "Parsing failed"
+end
+
+cl34.parse do |tu, cursor, parent|
+  if cl34.cursor_in_main_file?(cursor)
+    puts cursor.spelling if cursor.kind == Clangc::CursorKind::FUNCTION_DECL
+  end
+  Clangc::ChildVisitResult::RECURSE
+end
