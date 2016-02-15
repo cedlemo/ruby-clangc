434c93d80d85533a16a8c66a29bbff28c4ba0c88
cedlemo <cedlemo@gmx.com>
2016-02-15 15:33:46 +0100

    Improve tests for Clangc::SourceLocation#file_location

--- test/SourceLocation_tests.rb
+++ test/SourceLocation_tests.rb
@@ -92,11 +92,19 @@ class TestSourceLocation < MiniTest::Test
     #offset
     assert_equal 0, file_location[3]
   end
-#  def test_SourceRange_get_end
-#    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_TWO_ERRORS,CLANG_HEADERS_PATH)
-#    diagnostics = tu.diagnostics
-#    range_number = tu.diagnostics[1].num_ranges
-#    source_range = diagnostics[1].source_ranges[range_number - 1]
-#    assert_instance_of Clangc::SourceLocation, source_range.end 
-#  end
+  def test_SourceLocation_file_location_start_end
+  tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE, CLANG_HEADERS_PATH)
+    Clangc.visit_children(cursor: tu.cursor) do |cursor, parent| 
+      if cursor.location.spelling[0].name == SOURCE_FILE  
+        if cursor.kind == Clangc::CursorKind::STRUCT_DECL 
+          range = cursor.extent
+          _file, start_line, start_column = range.start.file_location
+          _file, stop_line, stop_column = range.end.file_location
+          assert_equal([3, 9], [start_line, start_column], range.start.spelling)
+          assert_equal([6, 4], [stop_line, stop_column], range.end.spelling)
+        end
+      end
+      Clangc::ChildVisitResult::RECURSE
+    end
+  end
 end
