f9c2bf27244e25f85654300bddb8fa3b7b349417
cedlemo <cedlemo@gmx.com>
2016-02-15 15:24:40 +0100

    Improve tests for Clangc::SourceRange#start and Clangc::SourceRange#end methods

--- test/SourceRange_tests.rb
+++ test/SourceRange_tests.rb
@@ -60,4 +60,20 @@ class TestSourceRange < MiniTest::Test
     source_range = diagnostics[1].source_ranges[range_number - 1]
     assert_instance_of Clangc::SourceLocation, source_range.end 
   end
+  
+  def test_SourceRange_start_end
+  tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE, CLANG_HEADERS_PATH)
+    Clangc.visit_children(cursor: tu.cursor) do |cursor, parent| 
+      if cursor.location.spelling[0].name == SOURCE_FILE  
+        if cursor.kind == Clangc::CursorKind::STRUCT_DECL 
+          range = cursor.extent
+          _file, start_line, start_column = range.start.spelling
+          _file, stop_line, stop_column = range.end.spelling
+          assert_equal([3, 9], [start_line, start_column], range.start.spelling)
+          assert_equal([6, 4], [stop_line, stop_column], range.end.spelling)
+        end
+      end
+      Clangc::ChildVisitResult::RECURSE
+    end
+  end
 end
