08148fb24a015f7ad4fa0dbae01e9a865d60339e
cedlemo <cedlemo@gmx.com>
2016-02-17 18:54:54 +0100

    Finish test for Clangc::Type#class_type

--- test/Type_tests.rb
+++ test/Type_tests.rb
@@ -285,15 +285,16 @@ class TestTypeUsage < MiniTest::Test
     end
   end
   def test_Type_get_class_type
-#    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_MEMBER_PTR, ["-x", "c++", "--std", "c++11"] + CLANG_HEADERS_PATH)
-#    Clangc.visit_children(cursor: tu.cursor) do |cursor, parent| 
-      #if cursor.location.spelling[0].name == SOURCE_FILE_MEMBER_PTR
-#        class_type = cursor.type.class_type
-#        assert_equal(Clangc::TypeKind::INVALID, class_type.kind, cursor.spelling)
-      #end
-      # TODO
-#      Clangc::ChildVisitResult::RECURSE
-#    end
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_MEMBER_PTR, ["-x", "c++", "--std", "c++11"] + CLANG_HEADERS_PATH)
+    Clangc.visit_children(cursor: tu.cursor) do |cursor, parent| 
+     if cursor.location.spelling[0].name == SOURCE_FILE_MEMBER_PTR
+        if cursor.spelling == "pwCaption"
+        class_type = cursor.type.class_type
+        assert_equal(Clangc::TypeKind::RECORD, class_type.kind, cursor.spelling)
+        end
+     end
+      Clangc::ChildVisitResult::RECURSE
+    end
   end
   def test_Type_get_offset_of
     tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_TYPE_OFFSET_OF, ["-x", "c++", "--std", "c++11"] + CLANG_HEADERS_PATH)
