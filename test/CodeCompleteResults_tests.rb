548d0bbe96b11ba9f50c8192b5e2c08e106c6570
cedlemo <cedlemo@gmx.com>
2016-02-26 12:36:29 +0100

    Add test for Clangc::CodeCompleteResults#result method

--- test/CodeCompleteResults_tests.rb
+++ test/CodeCompleteResults_tests.rb
@@ -22,4 +22,16 @@ class TestCodeCompleteResults < MiniTest::Test
     assert_instance_of(Fixnum, complete_results.num_results)
 
   end
+  def test_CodeCompleteResults_get_result
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_COMPLETION_STRING, CLANG_HEADERS_PATH)
+    line = 12 
+    column = 5 
+    options = Clangc.default_code_complete_options
+    complete_results = tu.code_complete_at(SOURCE_FILE_COMPLETION_STRING,
+                                           line,
+                                           column,
+                                           options)
+    # TODO
+    assert_instance_of(Clangc::CompletionResult, complete_results.result(0))
+  end
 end
