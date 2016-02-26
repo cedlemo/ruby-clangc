8aa23039d55cd3f34c53f0af823b79fd8f69df9b
cedlemo <cedlemo@gmx.com>
2016-02-26 15:14:46 +0100

    Add test for Clangc::CodeCompleteResults#diagnostic method

--- test/CompletionResult_tests.rb
+++ test/CompletionResult_tests.rb
@@ -38,4 +38,42 @@ class TestCompletionResult < MiniTest::Test
     # TODO
     assert_instance_of(Clangc::CompletionString, completion_result.completion_string)  
   end
+  def test_CompletionResult_get_container_usr
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_COMPLETION_STRING, CLANG_HEADERS_PATH)
+    line = 12 
+    column = 5 
+    options = Clangc.default_code_complete_options
+    complete_results = tu.code_complete_at(SOURCE_FILE_COMPLETION_STRING,
+                                           line,
+                                           column,
+                                           options)
+    # TODO
+    assert_instance_of(String, complete_results.container_usr)
+    assert_equal("", complete_results.container_usr)
+  end
+  def test_CompletionResult_get_num_diagnostics
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_COMPLETION_STRING, CLANG_HEADERS_PATH)
+    line = 12 
+    column = 5 
+    options = Clangc.default_code_complete_options
+    complete_results = tu.code_complete_at(SOURCE_FILE_COMPLETION_STRING,
+                                           line,
+                                           column,
+                                           options)
+    assert_equal(8, complete_results.num_diagnostics)
+  end
+  def test_CompletionResult_get_diagnostic
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_COMPLETION_STRING, CLANG_HEADERS_PATH)
+    line = 12 
+    column = 5 
+    options = Clangc.default_code_complete_options
+    complete_results = tu.code_complete_at(SOURCE_FILE_COMPLETION_STRING,
+                                           line,
+                                           column,
+                                           options)
+    # TODO
+    assert_instance_of(Clangc::Diagnostic, complete_results.diagnostic(0))
+    assert_equal("redefinition of 'ptr' with a different type: 'int' vs 'struct data *'", complete_results.diagnostic(0).spelling)
+
+  end
 end
