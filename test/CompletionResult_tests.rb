79153c2bbdf71bb0b2c8ac035bc1a1204596e65a
cedlemo <cedlemo@gmx.com>
2016-02-26 14:21:36 +0100

    Add test for Clangc::CompletionResult#cursor_kind method

---
+++ test/CompletionResult_tests.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require "minitest/autorun"
+require "clangc"
+require "fileutils"
+require "#{File.expand_path(File.dirname(__FILE__))}/clangc_utils.rb"
+
+class TestCompletionResult < MiniTest::Test
+  include ClangcUtils
+  def setup
+    @cindex = Clangc::Index.new(false, false)
+  end
+  def test_CompletionResult_get_cursor_kind
+    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_COMPLETION_STRING, CLANG_HEADERS_PATH)
+    line = 12 
+    column = 5 
+    options = Clangc.default_code_complete_options
+    complete_results = tu.code_complete_at(SOURCE_FILE_COMPLETION_STRING,
+                                           line,
+                                           column,
+                                           options)
+    completion_result = complete_results.result(0)
+    # TODO
+    found = false
+    Clangc::CursorKind.constants.each do |c|
+      found = true if completion_result.cursor_kind == Clangc::CursorKind.const_get(c)  
+    end
+  end
+end
--- tools/reinstallgem.sh
+++ tools/reinstallgem.sh
@@ -6,7 +6,7 @@ gem build $DIR/../clangc.gemspec
 gem install --user-install --debug --verbose $W_DIR/clangc-0.0.1.gem -- --enable-debug
 for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" \
   "SourceLocation" "Cursor" "Type" "CursorSet" "CompletionString" "Module"\
-  "CodeCompleteResults"; do
+  "CodeCompleteResults" "CompletionResult"; do
   echo ""
   echo "############> ${N}_tests.rb <###############"
   ruby "${DIR}/../test/${N}_tests.rb" --pride
