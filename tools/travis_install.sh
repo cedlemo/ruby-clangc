749e4ab116c166398dcfff3c14f90323e463b3f5
cedlemo <cedlemo@gmx.com>
2016-03-02 14:29:21 +0100

    Add better script for installation

---
+++ tools/travis_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+W_DIR=$(pwd)
+cd ext/clangc
+ruby extconf.rb
+cd $W_DIR
+sudo gem uninstall clangc
+gem build $DIR/../clangc.gemspec
+sudo gem install $W_DIR/clangc-0.0.1.gem
+
+for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" \
+  "SourceLocation" "Cursor" "Type" "CursorSet" "CompletionString" "Module"\
+  "CodeCompleteResults" "CompletionResult"; do
+  echo ""
+  echo "############> ${N}_tests.rb <###############"
+  ruby "${DIR}/../test/${N}_tests.rb" --pride
+  echo ""
+done
+
+
