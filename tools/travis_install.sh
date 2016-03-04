94bc22ae43183f939485d23d3e1f5d1b7cebd3f5
cedlemo <cedlemo@gmx.com>
2016-03-04 10:05:02 +0100

    Simplify travis configuration for testing purpose

--- tools/travis_install.sh
+++ tools/travis_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 W_DIR=$(pwd)
 cd ext/clangc
@@ -6,7 +6,7 @@ ruby extconf.rb
 cd $W_DIR
 gem uninstall clangc
 gem build $DIR/../clangc.gemspec
-gem install --user-install --verbose --local clangc-0.0.1.gem
+gem install --user-install --debug --verbose --local clangc-0.0.1.gem
 
 for N in "Clangc" "Index" "TranslationUnit" "Diagnostic" "File" "SourceRange" \
   "SourceLocation" "Cursor" "Type" "CursorSet" "CompletionString" "Module"\
