8dc161e8281f4cb2274f48af71c6db878adf50eb
cedlemo <cedlemo@gmx.com>
2016-03-09 13:16:51 +0100

    Clean code

--- ext/clangc/macros.h
+++ ext/clangc/macros.h
@@ -10,88 +10,135 @@
 #include "clang-c/Index.h"
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define SENTINEL(M, ...) printf("[SENTINEL] (%s:%s:%d: errno: %s) " M "\n",__FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define LOG_ERR(M, ...) fprintf(stderr,  "[ERROR] (%s:%s:%d: errno: %s) " M "\n", __FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define LOG_WARN(M, ...) fprintf(stderr, "[WARN]  (%s:%s:%d: errno: %s) " M "\n", __FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define LOG_INFO(M, ...) fprintf(stderr, "[INFO]  (%s:%s:%d) " M "\n", __FILE__,__FUNCTION__ , __LINE__, ##__VA_ARGS__)
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; }
+#define SENTINEL(M, ...)                                                       \
+    printf("[SENTINEL] (%s:%s:%d: errno: %s) " M "\n",                         \
+           __FILE__,                                                           \
+           __FUNCTION__,                                                       \
+           __LINE__,                                                           \
+           clean_errno(),                                                      \
+           ##__VA_ARGS__)
+#define LOG_ERR(M, ...)                                                        \
+    fprintf(stderr,                                                            \
+            "[ERROR] (%s:%s:%d: errno: %s) " M "\n",                           \
+            __FILE__,                                                          \
+            __FUNCTION__,                                                      \
+            __LINE__,                                                          \
+            clean_errno(),                                                     \
+            ##__VA_ARGS__)
+#define LOG_WARN(M, ...)                                                       \
+    fprintf(stderr,                                                            \
+            "[WARN]  (%s:%s:%d: errno: %s) " M "\n",                           \
+            __FILE__,                                                          \
+            __FUNCTION__,                                                      \
+            __LINE__,                                                          \
+            clean_errno(),                                                     \
+            ##__VA_ARGS__)
+#define LOG_INFO(M, ...)                                                       \
+    fprintf(stderr,                                                            \
+            "[INFO]  (%s:%s:%d) " M "\n",                                      \
+            __FILE__,                                                          \
+            __FUNCTION__,                                                      \
+            __LINE__,                                                          \
+            ##__VA_ARGS__)
+#define check(A, M, ...)                                                       \
+    if (!(A))                                                                  \
+    {                                                                          \
+        log_err(M, ##__VA_ARGS__);                                             \
+        errno = 0;                                                             \
+    }
 #define check_mem(A) check((A), "Out of memory.")
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#define check_debug(A, M, ...)                                                 \
+    if (!(A))                                                                  \
+    {                                                                          \
+        debug(M, ##__VA_ARGS__);                                               \
+        errno = 0;                                                             \
+        goto error;                                                            \
+    }
 /*****************/
 /*Common C macros*/
 /*****************/
-#define CARRAY_LEN(A)   (sizeof(A) / sizeof((A)[0]));
-#define CHECK_IN_RANGE(A, MIN, MAX)\
-  if (A < MIN)\
-    A = MIN;\
-  if (A > MAX)\
-    A = MAX;
+#define CARRAY_LEN(A) (sizeof(A) / sizeof((A)[0]));
+#define CHECK_IN_RANGE(A, MIN, MAX)                                            \
+    if (A < MIN) A = MIN;                                                      \
+    if (A > MAX) A = MAX;
 
 /***************************/
 /*Ruby arguments to C value*/
 /***************************/
-//ruby array of string to C array
-#define RARRAY_OF_STRINGS_2_C(args)\
-Check_Type(args, T_ARRAY);\
-int len = RARRAY_LEN(args);\
-const char * c_args[len];\
-if(len > 0)\
-{\
-  int j = 0;\
-  for (j=0; j< len; j++) {\
-    VALUE arg = rb_ary_entry(args, j);\
-    c_args[j] = StringValueCStr(arg);\
-  }\
-}
-//ruby boolean value to C int
+// ruby array of string to C array
+#define RARRAY_OF_STRINGS_2_C(args)                                            \
+    Check_Type(args, T_ARRAY);                                                 \
+    int len = RARRAY_LEN(args);                                                \
+    const char *c_args[len];                                                   \
+    if (len > 0)                                                               \
+    {                                                                          \
+        int j = 0;                                                             \
+        for (j = 0; j < len; j++)                                              \
+        {                                                                      \
+            VALUE arg = rb_ary_entry(args, j);                                 \
+            c_args[j] = StringValueCStr(arg);                                  \
+        }                                                                      \
+    }
+// ruby boolean value to C int
 static inline int rbool_2_int(VALUE rval)
 {
-  return (int) ((rval == Qtrue)? 1 : 0);
+    return (int) ((rval == Qtrue) ? 1 : 0);
 }
 #define RBOOL_2_INT(a) rbool_2_int(a)
 
-//Ruby string to C char 
-static inline char * rstring_2_char(VALUE rval)
+// Ruby string to C char
+static inline char *rstring_2_char(VALUE rval)
 {
-  VALUE rb_string = StringValue(rval);
-  return StringValueCStr(rval);
+    return StringValueCStr(rval);
 }
 #define RSTRING_2_CHAR(rval) rstring_2_char(rval)
 
 /****************/
 /*Classes Macros*/
 /****************/
-//Check argument
+// Check argument
 static inline void check_arg_type(VALUE arg, char *class_name)
 {
-  Check_Type(arg, T_DATA);
-  VALUE mModule = rb_const_get(rb_cObject, rb_intern("Clangc"));
-  VALUE cKlass =  rb_const_get(mModule, rb_intern(class_name));
-  char *message;
-  int len = asprintf(&message, "Clangc::%s Object was expected",class_name); 
-  if (cKlass != rb_funcall(arg, rb_intern("class"), 0))
-  {
-    rb_raise(rb_eTypeError, message);
-    free(message);
-  }
+    VALUE mModule;
+    VALUE cKlass;
+    char *message;
+    int len;
+
+    Check_Type(arg, T_DATA);
+    mModule = rb_const_get(rb_cObject, rb_intern("Clangc"));
+    cKlass = rb_const_get(mModule, rb_intern(class_name));
+    
+    len = asprintf(&message, "Clangc::%s Object was expected", class_name);
+    
+    if (cKlass != rb_funcall(arg, rb_intern("class"), 0))
+    {
+        if (len != -1)
+        {
+            rb_raise(rb_eTypeError, message);
+            free(message);
+        }
+        else
+            rb_raise(rb_eTypeError, "Not the object type expected");
+    }
 }
 
 #define CHECK_ARG_TYPE(arg, classname) check_arg_type(arg, #classname)
 
-//Create a new instance without args and get its data
-#define R_GET_CLASS_DATA(module_name, class_name, instance, data_ptr)\
-VALUE mModule = rb_const_get(rb_cObject, rb_intern(module_name));\
-  VALUE cKlass = rb_const_get(mModule, rb_intern(#class_name));\
-  instance = rb_class_new_instance(0, NULL, cKlass);\
-  Data_Get_Struct(instance, class_name ##_t, data_ptr);
+// Create a new instance without args and get its data
+#define R_GET_CLASS_DATA(module_name, class_name, instance, data_ptr)          \
+    VALUE mModule = rb_const_get(rb_cObject, rb_intern(module_name));          \
+    VALUE cKlass = rb_const_get(mModule, rb_intern(#class_name));              \
+    instance = rb_class_new_instance(0, NULL, cKlass);                         \
+    Data_Get_Struct(instance, class_name##_t, data_ptr);
 
-//Create a new instance with args and get its data
-#define R_GET_CLASS_WITH_ARGS_DATA(module_name, class_name, instance, args, data_type, data_ptr)\
-VALUE mModule = rb_const_get(rb_cObject, rb_intern(module_name));\
-  VALUE cKlass = rb_const_get(mModule, rb_intern(class_name));\
-  int args_n=CARRAY_LEN(args)\
-  instance = rb_class_new_instance(args_len, args, cKlass);\
-  Data_Get_Struct(instance, data_type, data_ptr);
+// Create a new instance with args and get its data
+#define R_GET_CLASS_WITH_ARGS_DATA(                                            \
+    module_name, class_name, instance, args, data_type, data_ptr)              \
+    VALUE mModule = rb_const_get(rb_cObject, rb_intern(module_name));          \
+    VALUE cKlass = rb_const_get(mModule, rb_intern(class_name));               \
+    int args_n = CARRAY_LEN(args) instance =                                   \
+        rb_class_new_instance(args_len, args, cKlass);                         \
+    Data_Get_Struct(instance, data_type, data_ptr);
 
 /* For later.
 #define NEW_CLASS_INSTANCE(class_name)\
@@ -109,10 +156,12 @@ NEW_CLASS_INSTANCE(TranslationUnit)
 /************************/
 static inline VALUE cxstring_2_rval(CXString str)
 {
-  const char * c_string = (clang_getCString(str) != NULL)? clang_getCString(str) : "";
-  VALUE r_string = rb_str_new2(c_string);
-  clang_disposeString(str);
-  return r_string;
+    VALUE r_string;
+    const char *c_string =
+        (clang_getCString(str) != NULL) ? clang_getCString(str) : "";
+    r_string = rb_str_new2(c_string);
+    clang_disposeString(str);
+    return r_string;
 }
 #define CUINT_2_NUM(c_val) UINT2NUM(c_val)
 #define CINT_2_NUM(c_val) INT2NUM(c_val)
@@ -122,4 +171,4 @@ static inline VALUE cxstring_2_rval(CXString str)
 #define EQ_1_2_RVAL(c_val) ((c_val == 1) ? Qtrue : Qfalse)
 #define CXSTR_2_RVAL(str) (cxstring_2_rval(str))
 
-#endif //MACROS_H
+#endif // MACROS_H
