194fe8a668852776165a384e15193fa0ce57a037
cedlemo <cedlemo@gmx.com>
2016-03-21 11:18:36 +0100

    Check if clang 3.8 is dipo for travis

--- tools/travis_before_install.sh
+++ tools/travis_before_install.sh
@@ -13,6 +13,10 @@ case "$CLANG_SRC_VER" in
     apt-get update -qq
     apt-get install -qq llvm-dev libclang-3.7-dev libclang1-3.7 libclang-common-3.7-dev build-essential ruby-dev
     ln -s /usr/lib/x86_64-linux-gnu/libclang-3.7.so /usr/lib/x86_64-linux-gnu/libclang.so;;
+  38)
+    apt-get update -qq
+    apt-get install -qq llvm-dev libclang-3.8-dev libclang1-3.8 libclang-common-3.8-dev build-essential ruby-dev
+    ln -s /usr/lib/x86_64-linux-gnu/libclang-3.8.so /usr/lib/x86_64-linux-gnu/libclang.so;;
   *)
     apt-get update -qq
     apt-get install -qq llvm-dev libclang-3.5-dev libclang1-3.5 libclang-common-3.5-dev build-essential ruby-dev
