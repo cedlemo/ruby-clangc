a08cfc35fb158c7fb56061e57ed1a0f1b4e2042c
cedlemo <cedlemo@gmx.com>
2016-05-06 21:15:41 +0200

    Add tests for usage of symbols with Clangc::Index#global_options=

--- test/Index_tests.rb
+++ test/Index_tests.rb
@@ -4,16 +4,19 @@ require "clangc"
 
 class TestIndexCreation < MiniTest::Test
   def test_new_index_dont_exclude_decl_from_pch_and_dont_display_diagnostics
-    assert_equal Clangc::Index, Clangc::Index.new(false, false).class
+    assert_equal(Clangc::Index, Clangc::Index.new(false, false).class)
   end
+
   def test_new_index_exclude_decl_from_pch_and_dont_display_diagnostics
-    assert_equal Clangc::Index, Clangc::Index.new(true, false).class
+    assert_equal(Clangc::Index, Clangc::Index.new(true, false).class)
   end
+
   def test_new_index_dont_exclude_decl_from_pch_and_display_diagnostics
-    assert_equal Clangc::Index, Clangc::Index.new(false, true).class
+    assert_equal(Clangc::Index, Clangc::Index.new(false, true).class)
   end
+
   def test_new_index_exclude_decl_from_pch_and_display_diagnostics
-    assert_equal Clangc::Index, Clangc::Index.new(true, true).class
+    assert_equal(Clangc::Index, Clangc::Index.new(true, true).class)
   end
 end
 
@@ -25,32 +28,57 @@ class TestIndexGlobalOptions < MiniTest::Test
   def setup
     @cindex = Clangc::Index.new(true, true)
   end
+
   def test_index_set_global_options_None
     flags = Clangc::GlobalOptFlags::NONE
     @cindex.global_options = flags
-    assert_equal flags, @cindex.global_options
+    assert_equal(flags, @cindex.global_options)
   end
+
+  def test_index_set_global_options_None_sym
+    flags = Clangc::GlobalOptFlags::NONE
+    @cindex.global_options = :none
+    assert_equal(flags, @cindex.global_options)
+  end
+
   def test_index_set_global_options_editing
     flags =  Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_EDITING
     @cindex.global_options = flags
-    assert_equal flags, @cindex.global_options
+    assert_equal(flags, @cindex.global_options)
   end
+
+  def test_index_set_global_options_editing_sym
+    flags =  Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_EDITING
+    @cindex.global_options = :thread_background_priority_for_editing
+    assert_equal(flags, @cindex.global_options)
+  end
+
+  def test_index_set_global_options_editing_and_indexing_sym
+    flags = Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_EDITING |
+            Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_INDEXING
+    @cindex.global_options = [:thread_background_priority_for_editing,
+                              :thread_background_priority_for_indexing]
+    assert_equal(flags, @cindex.global_options)
+  end
+
   def test_index_set_global_options_and_editing
     flags =  @cindex.global_options |
              Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_EDITING
     @cindex.global_options = flags
-    assert_equal flags, @cindex.global_options
+    assert_equal(flags, @cindex.global_options)
   end
+
   def test_index_set_global_options_and_indexing
     flags =  @cindex.global_options |
              Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_INDEXING
     @cindex.global_options = flags
-    assert_equal flags, @cindex.global_options
+    assert_equal(flags, @cindex.global_options)
   end
+
   def test_index_set_global_options_all
     flags =   @cindex.global_options |
               Clangc::GlobalOptFlags::THREAD_BACKGROUND_PRIORITY_FOR_ALL
     @cindex.global_options = flags
-    assert_equal flags, @cindex.global_options
+    assert_equal(flags, @cindex.global_options)
   end
 end
