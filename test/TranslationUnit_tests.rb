b1f365566a5fd0e4da212745cdaf15f065f82644
cedlemo <cedlemo@gmx.com>
2016-05-07 11:08:43 +0200

    Add test for sym usage as arguments with Clangc::Index#parse_translation_unit* methods

--- test/TranslationUnit_tests.rb
+++ test/TranslationUnit_tests.rb
@@ -9,70 +9,71 @@ class TestTranslationUnitCreation < MiniTest::Test
 
   def setup
     @cindex = Clangc::Index.new(false, false)
-    system *%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE})
+    system(*%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE}))
   end
 
   def teardown
     FileUtils.rm_f(AST_FILE)
   end
 
-  def test_create_TU_from_source_file
+  def test_create_tu_from_source_file
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_create_TU_from_source_file
+  def test_fail_create_tu_from_source_file
     tu = @cindex.create_translation_unit(source: BAD_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_nil(tu)
   end
 
-  def test_create_TU_from_source_file_empty_cmdline
+  def test_create_tu_from_source_file_empty_cmdline
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_create_TU_from_source_file_empty_cmdline
+  def test_fail_create_tu_from_source_file_empty_cmdline
     tu = @cindex.create_translation_unit(source: BAD_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_nil(tu)
   end
 
-  def test_create_TU_from_source_file_in_cmdline
-    tu = @cindex.create_translation_unit(args: [SOURCE_FILE] + CLANG_HEADERS_PATH)
+  def test_create_tu_from_source_file_in_cmdline
+    tu = @cindex.create_translation_unit(args: [SOURCE_FILE] +
+                                               CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_create_TU_from_source_file_in_cmdline
+  def test_fail_create_tu_from_source_file_in_cmdline
     tu = @cindex.create_translation_unit(args: [BAD_FILE] + CLANG_HEADERS_PATH)
     assert_nil(tu)
   end
 
-  def test_create_TU
+  def test_create_tu
     tu = @cindex.create_translation_unit(ast: AST_FILE)
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_create_TU
+  def test_fail_create_tu
     tu = @cindex.create_translation_unit(ast: BAD_FILE)
     assert_nil(tu)
   end
 
   # return an error code if the Translation Unit creation fail
-  def test_create_TU2
+  def test_create_tu2
     tu = @cindex.create_translation_unit(ast: AST_FILE, error: true)
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_create_TU2
+  def test_fail_create_tu2
     tu = @cindex.create_translation_unit(ast: BAD_FILE, error: true)
     assert_equal(Integer, tu.class.superclass)
     assert_equal(Clangc::ErrorCode::FAILURE, tu)
   end
 
-  def test_parse_TU
+  def test_parse_tu
     options = Clangc::TranslationUnit_Flags::NONE
     tu = @cindex.parse_translation_unit(source: SOURCE_FILE,
                                         args: CLANG_HEADERS_PATH,
@@ -80,7 +81,14 @@ class TestTranslationUnitCreation < MiniTest::Test
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_fail_parse_TU
+  def test_parse_tu_sym
+    tu = @cindex.parse_translation_unit(source: SOURCE_FILE,
+                                        args: CLANG_HEADERS_PATH,
+                                        flags: :none)
+    assert_instance_of(Clangc::TranslationUnit, tu)
+  end
+
+  def test_fail_parse_tu
     options = Clangc::TranslationUnit_Flags::NONE
     tu = @cindex.parse_translation_unit(source: BAD_FILE,
                                         args: CLANG_HEADERS_PATH,
@@ -89,7 +97,7 @@ class TestTranslationUnitCreation < MiniTest::Test
   end
 
   # return an error code if the Translation Unit creation fail
-  def test_parse_TU2
+  def test_parse_tu2
     options = Clangc::TranslationUnit_Flags::NONE
     tu = @cindex.parse_translation_unit(source: SOURCE_FILE,
                                         args: CLANG_HEADERS_PATH,
@@ -97,7 +105,14 @@ class TestTranslationUnitCreation < MiniTest::Test
     assert_instance_of(Clangc::TranslationUnit, tu)
   end
 
-  def test_failparse_TU2
+  def test_parse_tu2_sym
+    tu = @cindex.parse_translation_unit(source: SOURCE_FILE,
+                                        args: CLANG_HEADERS_PATH,
+                                        flags: :none, error: true)
+    assert_instance_of(Clangc::TranslationUnit, tu)
+  end
+
+  def test_failparse_tu2
     options = Clangc::TranslationUnit_Flags::NONE
     tu = @cindex.parse_translation_unit(source: BAD_FILE,
                                         args: CLANG_HEADERS_PATH,
@@ -111,74 +126,74 @@ class TestTranslationUnitUsage < MiniTest::Test
   include ClangcUtils
   def setup
     @cindex = Clangc::Index.new(false, false)
-    system *%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE})
+    system(*%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE}))
   end
 
   def teardown
     FileUtils.rm_f(AST_FILE)
   end
 
-  def test_TU_get_num_diagnostic_zero
+  def test_tu_get_num_diagnostic_zero
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(0, tu.diagnostics_num)
   end
 
-  def test_TU_get_num_diagnostic_one
+  def test_tu_get_num_diagnostic_one
     tu = @cindex.create_translation_unit(source: SOURCE_FILE_ONE_ERROR,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(1, tu.diagnostics_num)
   end
 
-  def test_TU_get_default_save_options
+  def test_tu_get_default_save_options
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(Clangc::SaveTranslationUnit_Flags::NONE,
                  tu.default_save_options)
   end
 
-  def test_TU_get_spelling
+  def test_tu_get_spelling
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(SOURCE_FILE, tu.spelling)
   end
 
-  def test_TU_get_default_reparse_options
+  def test_tu_get_default_reparse_options
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(Clangc::Reparse_Flags::NONE, tu.default_reparse_options)
   end
 
-  def test_TU_get_diagnostic_one
+  def test_tu_get_diagnostic_one
     tu = @cindex.create_translation_unit(source: SOURCE_FILE_ONE_ERROR,
                                          args: CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::Diagnostic, tu.diagnostic(0))
   end
 
-  def test_TU_get_file_that_exists
+  def test_tu_get_file_that_exists
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::File, tu.file(SOURCE_FILE))
   end
 
-  def test_TU_get_file_that_doesnt_exist
+  def test_tu_get_file_that_doesnt_exist
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_equal(nil, tu.file(BAD_FILE))
   end
 
-  def test_TU_get_cursor
+  def test_tu_get_cursor
     tu = @cindex.create_translation_unit(source: SOURCE_FILE,
                                          args: CLANG_HEADERS_PATH)
     assert_instance_of(Clangc::Cursor, tu.cursor)
   end
 
-  def test_TU_get_module
-    # TODO
+  def test_tu_get_module
+    # TODO: find a working test case
   end
 
-  def test_TU_code_complete_at
-    # TODO check this.
+  def test_tu_code_complete_at
+    # TODO: check this and clean.
     tu = @cindex.create_translation_unit(source: SOURCE_FILE_COMPLETION_STRING,
                                          args: CLANG_HEADERS_PATH)
     line = 1
@@ -206,13 +221,13 @@ class TestTranslationUnitUsage < MiniTest::Test
     all_chunks_ref = [["const"],
                       ["volatile"],
                       ["restrict"]
-                      ]
+                     ]
     assert_equal(all_chunks_ref, all_chunks)
     assert_equal(Clangc::CompletionChunkKind::TYPED_TEXT,
                  complete_results.results[0].completion_string.chunk_kind(0))
   end
 
-  def test_TU_reparse
+  def test_tu_reparse
     tu = @cindex.create_translation_unit(source: SOURCE_FILE_COMPLETION_STRING,
                                          args: CLANG_HEADERS_PATH)
     options = tu.default_reparse_options
