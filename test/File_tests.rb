73047ae623732c2ae909dbf712995b6a3913d2a9
cedlemo <cedlemo@gmx.com>
2016-05-12 16:23:03 +0200

    File_tests.rb: Clean and fix style issue

--- test/File_tests.rb
+++ test/File_tests.rb
@@ -22,37 +22,46 @@ require "#{File.expand_path(File.dirname(__FILE__))}/clangc_utils.rb"
 
 class TestFile < MiniTest::Test
   include ClangcUtils
+
   def setup
     @cindex = Clangc::Index.new(false, false)
-    system *%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE})
-    @tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE, CLANG_HEADERS_PATH)
+    system(*%W(clang -emit-ast -o #{AST_FILE} #{SOURCE_FILE}))
+    @tu = @cindex.create_translation_unit(source: SOURCE_FILE,
+                                          args: CLANG_HEADERS_PATH)
   end
+
   def teardown
     FileUtils.rm_f(AST_FILE)
   end
-  def test_File_get_name
+
+  def test_file_get_name
     file = @tu.file(SOURCE_FILE)
-    assert_equal SOURCE_FILE, file.name
+    assert_equal(SOURCE_FILE, file.name)
   end
-  def test_File_time
+
+  def test_file_time
     file = @tu.file(SOURCE_FILE)
-    assert_equal File.mtime(SOURCE_FILE).asctime, file.mtime.asctime
+    assert_equal(File.mtime(SOURCE_FILE).asctime, file.mtime.asctime)
   end
-  def test_File_multiple_include_guarded_false
+
+  def test_file_multiple_include_guarded_false
     file = @tu.file(SOURCE_FILE)
-    assert_equal false, file.is_multiple_include_guarded
+    assert_equal(false, file.is_multiple_include_guarded)
   end
-  def test_File_multiple_include_guarded_true
-    tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE_WITH_INCLUDE_GUARD, CLANG_HEADERS_PATH)
+
+  def test_file_multiple_include_guarded_true
+    tu = @cindex.create_translation_unit(source: SOURCE_FILE_WITH_INCLUDE_GUARD,
+                                         args: CLANG_HEADERS_PATH)
     file = tu.file(SOURCE_FILE_WITH_INCLUDE_GUARD)
-    assert_equal true, file.is_multiple_include_guarded
+    assert_equal(true, file.is_multiple_include_guarded)
   end
+
   if Clangc.version =~ /clang version 3.(6|7)/
-    def test_File_equal_true
+    def test_file_equal_true
       file = @tu.file(SOURCE_FILE)
       another_file_object = @tu.file(SOURCE_FILE)
-      assert_equal false, file == another_file_object
-      assert_equal true, file.is_equal(another_file_object)
+      assert_equal(false, file == another_file_object)
+      assert_equal(true, file.is_equal(another_file_object))
     end
   end
 end
