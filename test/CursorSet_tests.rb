24db81009be7246fb287f4ab94440dca5d846106
cedlemo <cedlemo@gmx.com>
2016-05-16 10:57:45 +0200

    Clean code and fix style issues

--- test/CursorSet_tests.rb
+++ test/CursorSet_tests.rb
@@ -22,29 +22,31 @@ require "#{File.expand_path(File.dirname(__FILE__))}/clangc_utils.rb"
 
 class TestCursorSet < MiniTest::Test
   include ClangcUtils
+
   def setup
     @cindex = Clangc::Index.new(false, false)
-    @tu = @cindex.create_translation_unit_from_source_file(SOURCE_FILE, CLANG_HEADERS_PATH)
-end
-  def teardown
+    @tu = @cindex.create_translation_unit(source: SOURCE_FILE,
+                                          args: CLANG_HEADERS_PATH)
   end
-  def test_CursorSet_new
+
+  def test_cursor_set_new
     assert_instance_of Clangc::CursorSet, Clangc::CursorSet.new
   end
-  def test_CursorSet_contains
+
+  def test_cursor_set_contains
     cursor_set = Clangc::CursorSet.new
-    Clangc.visit_children(cursor: @tu.cursor) do |cursor, parent|
-      assert_equal false, cursor_set.contains(cursor)
+    Clangc.visit_children(cursor: @tu.cursor) do |cursor, _parent|
+      assert_equal(false, cursor_set.contains(cursor))
       Clangc::ChildVisitResult::RECURSE
     end
   end
-  def test_CursorSet_insert
-    cursor_set = Clangc::CursorSet.new
-    Clangc.visit_children(cursor: @tu.cursor) do |cursor, parent|
-      assert_equal true, cursor_set.insert(cursor)
-      assert_equal true, cursor_set.contains(cursor)
-      assert_equal false, cursor_set.insert(cursor)
 
+  def test_cursor_set_insert
+    cursor_set = Clangc::CursorSet.new
+    Clangc.visit_children(cursor: @tu.cursor) do |cursor, _parent|
+      assert_equal(true, cursor_set.insert(cursor))
+      assert_equal(true, cursor_set.contains(cursor))
+      assert_equal(false, cursor_set.insert(cursor))
       Clangc::ChildVisitResult::BREAK
     end
   end
